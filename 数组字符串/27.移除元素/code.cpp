443097780b3f77c0f6943a85ad230b4336374b7c
1ZhangMing <123637457+1ZhangMing@users.noreply.github.com>
2025-01-26 22:01:20 +0800

    Create code.cpp

---
+++ 数组字符串/27.移除元素/code.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        int inequalnum=0;
+        vector<int> temp(nums.size());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                temp[inequalnum]=nums[i];
+                inequalnum++;
+            }
+        }
+
+        for(int i=0;i<inequalnum;i++){
+            nums[i]=temp[i];
+        }
+        return inequalnum;
+    }
+};
+
+int main(){
+
+}
